848053da9e1a0a4c54035bf8002a71fe868d08f9
Anand Rajaram <anandrajaram21@gmail.com>
2021-09-22 18:49:47 +0530

    feat: WIP commit

--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -6,6 +6,31 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      scope: "repo",
     }),
   ],
+  secret: process.env.AUTH_SECRET,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+
+  callbacks: {
+    async redirect(url, baseUrl) {
+      return "/";
+    },
+    async jwt(token, user, account, profile, isNewUser) {
+      // Add access_token to the token right after signin
+      if (account) {
+        if (account.accessToken) {
+          token.accessToken = account.accessToken;
+        }
+      }
+      return token;
+    },
+    async session(session, token) {
+      // Add property to session, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 });
