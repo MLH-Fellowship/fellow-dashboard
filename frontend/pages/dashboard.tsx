5d7b8ba34b47e37139dc534135641992732d8127
Anand Rajaram <anandrajaram21@gmail.com>
2021-09-23 09:32:43 +0530

    feat: basic route protection for the dashboard

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,16 +1,31 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useSession } from "next-auth/client";
+import Sidebar from "../components/sidebar";
 
 export default function Dashboard() {
+  const [session, loading] = useSession();
+  if (session) {
+    return (
+      <>
+        <Head>
+          <title>Dashboard</title>
+        </Head>
+        <Sidebar pageTitle="Dashboard" githubData={undefined}>
+          <div>
+            <h1>Dashboard</h1>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </div>
+        </Sidebar>
+      </>
+    );
+  }
+
   return (
-    <div>
-      <Head>
-        <title>Dashboard</title>
-      </Head>
-      <h1>Dashboard</h1>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-    </div>
+    <Sidebar pageTitle="Dashboard" githubData={undefined}>
+      <h1>Not Signed In</h1>
+    </Sidebar>
   );
 }
