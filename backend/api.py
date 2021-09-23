2f12118da1ba6f0d99b9114415e09d755d99531f
Kok Rui <kokruiwong@gmail.com>
2021-09-23 10:22:09 +0800

    cleanup + pythonicize var names

--- backend/api.py
+++ backend/api.py
@@ -24,17 +24,15 @@ class Scratchpad(Resource):
 
 
 class GithubPodList(Resource):
-    def get(self, oAuthToken):
+    def get(self, oAuth_token):
         headers = {
             "Accept": "application/vnd.github.v3+json",
-            "Authorization": f"token {oAuthToken}",
+            "Authorization": f"token {oAuth_token}",
         }
         response = requests.get(
             "https://api.github.com/orgs/MLH-Fellowship/teams/mlh-fellows-batch-4/teams",
             headers=headers,
         )
-        print(headers)
-        print(response.json())
         return [
             {
                 "name": pod["name"],
@@ -44,29 +42,32 @@ class GithubPodList(Resource):
             for pod in response.json()
         ]
 
+
 class GithubDiscussionList(Resource):
-    def get(self, pod_slug, oAuthToken):
+    def get(self, pod_slug, oAuth_token):
         headers = {
             "Accept": "application/vnd.github.v3+json",
-            "Authorization": f"token {oAuthToken}",
+            "Authorization": f"token {oAuth_token}",
         }
         response = requests.get(
             f"https://api.github.com/orgs/MLH-Fellowship/teams/{pod_slug}/discussions",
             headers=headers,
         )
-        print(headers)
-        print(response.json())
         return [
             {
-                "title": discussion['title'],
-                "url": discussion['url'],
+                "title": discussion["title"],
+                "url": discussion["url"],
             }
             for discussion in response.json()
         ]
 
+
 api.add_resource(Scratchpad, "/scratchpad")
-api.add_resource(GithubPodList, "/github/list-pods/<string:oAuthToken>")
-api.add_resource(GithubDiscussionList, "/github/list-discussions/<string:pod_slug>/<string:oAuthToken>")
+api.add_resource(GithubPodList, "/github/list-pods/<string:oAuth_token>")
+api.add_resource(
+    GithubDiscussionList,
+    "/github/list-discussions/<string:pod_slug>/<string:oAuth_token>",
+)
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port=5000)
