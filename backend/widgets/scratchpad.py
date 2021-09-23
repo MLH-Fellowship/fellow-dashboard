5db3016a039181e52cb7aa0b20cf72e72123e3aa
Kok Rui <kokruiwong@gmail.com>
2021-09-23 19:38:11 +0800

    scratchpad post->put + implement get method

--- backend/widgets/scratchpad.py
+++ backend/widgets/scratchpad.py
@@ -1,15 +1,17 @@
 from flask_restful import Resource, reqparse
 
 scratchpad_parser = reqparse.RequestParser()
-scratchpad_parser.add_argument("text", required=True)
-scratchpad_parser.add_argument("id", required=True)
+scratchpad_parser.add_argument("text")
 
 
 class Scratchpad(Resource):
     db = {}  # temp
 
-    def post(self):
+    def put(self, id):
         args = scratchpad_parser.parse_args()
         self.db[args["id"]] = args["text"]
         print(self.db)
         return {args["id"]: self.db[args["id"]]}
+
+    def get(self, id):
+        return {id: self.db.get("id", "")}
