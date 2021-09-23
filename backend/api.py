a919e62bfe3c9de9c3a0200203607b9aa1acd635
Kok Rui <kokruiwong@gmail.com>
2021-09-23 10:46:04 +0800

    view date of latest standup discussion threat

--- backend/api.py
+++ backend/api.py
@@ -29,6 +29,7 @@ def handle_github_request_errors(response):
     if response.status_code == 404:
         abort(404, message="resource not found, check request arguments")
 
+
 class GithubPodList(Resource):
     def get(self, oAuth_token):
         headers = {
@@ -70,12 +71,38 @@ class GithubDiscussionList(Resource):
         ]
 
 
+class GithubViewSubmitLatestStandup(Resource):
+    def get(self, pod_slug, oAuth_token):
+        headers = {
+            "Accept": "application/vnd.github.v3+json",
+            "Authorization": f"token {oAuth_token}",
+        }
+        response = requests.get(
+            f"https://api.github.com/orgs/MLH-Fellowship/teams/{pod_slug}/discussions",
+            headers=headers,
+        )
+        handle_github_request_errors(response)
+        latest_standup_date = next(
+            (
+                discussion["title"][discussion["title"].find("(") + 1 : -1]
+                for discussion in response.json()
+                if discussion["title"].startswith("Standup (")
+            ), "No standup discussion threads found!"
+        )
+        return {"date": latest_standup_date}
+
+
 api.add_resource(Scratchpad, "/scratchpad")
 api.add_resource(GithubPodList, "/github/list-pods/<string:oAuth_token>")
 api.add_resource(
     GithubDiscussionList,
     "/github/list-discussions/<string:pod_slug>/<string:oAuth_token>",
 )
+api.add_resource(
+    GithubViewSubmitLatestStandup,
+    "/github/standup/<string:pod_slug>/<string:oAuth_token>",
+)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port=5000)
